ec36dc662db2e45984b186d1bdf49e701189371f
krisg23 <cristhian.gonzalez@udea.edu.co>
2020-04-27 00:00:24 -0500

    Update Index Page

--- 0/code/script.js
+++ 0/code/script.js
@@ -24,7 +24,7 @@ var monthDirection = 0;
 
 var names = ["JavaScript Drum Kit", "JS and CSS Clock", "CSS Variables", "Array Cardio Day 1", "Flex Panels", "Type Ahead", "Array Cardio Day 2", "HTML5 Canvas", "Dev Tools Domination", "Hold Shift and Check Checkboxes", "Custom Video Player", "Key Sequence Detection", "Slide in on Scroll", "JavaScript References VS Copying", "LocalStorage", "Mouse Move Shadow", "Sort Without Articles", "Adding Up Times with Reduce", "Webcam Fun", "Speech Detection", "Geolocation", "Follow Along Link Highlighter", "Speech Synthesis", "Sticky Nav", "Event Capture", "Stripe Follow Along Nav", "Click and Drag", "Video Speed Controller", "Countdown Timer", "Whack A Mole"];
 var thumbs = ["thumbs/thumb1.jpg", "thumbs/thumb2.jpg", "thumbs/thumb3.jpg", "thumbs/thumb4.jpg", "thumbs/thumb5.jpg", "thumbs/thumb6.jpg", "thumbs/thumb7.jpg", "thumbs/thumb8.jpg", "thumbs/thumb9.jpg", "thumbs/thumb10.jpg", "thumbs/thumb11.jpg", "thumbs/thumb12.jpg", "thumbs/thumb13.jpg", "thumbs/thumb14.jpg", "thumbs/thumb15.jpg", "thumbs/thumb16.jpg", "thumbs/thumb17.jpg", "thumbs/thumb18.jpg", "thumbs/thumb19.jpg", "thumbs/thumb20.jpg", "thumbs/thumb21.jpg", "thumbs/thumb22.jpg", "thumbs/thumb23.jpg", "thumbs/thumb24.jpg", "thumbs/thumb25.jpg", "thumbs/thumb26.jpg", "thumbs/thumb27.jpg", "thumbs/thumb28.jpg", "thumbs/thumb29.jpg", "thumbs/thumb30.jpg"]
-var routes = ["", "https://krisg23.github.io/Javascript30/reto1/", "https://krisg23.github.io/Javascript30/reto2/", "https://krisg23.github.io/Javascript30/reto3/", "https://krisg23.github.io/Javascript30/reto4/", "https://krisg23.github.io/Javascript30/reto5/", "https://krisg23.github.io/Javascript30/reto6/", "https://krisg23.github.io/Javascript30/reto7/", "https://krisg23.github.io/Javascript30/reto8/", "https://krisg23.github.io/Javascript30/reto9/", "https://krisg23.github.io/Javascript30/reto10/", "https://krisg23.github.io/Javascript30/reto11/", "https://krisg23.github.io/Javascript30/reto12/", "https://krisg23.github.io/Javascript30/reto13/", "https://krisg23.github.io/Javascript30/reto14/", "https://krisg23.github.io/Javascript30/reto15/", "https://krisg23.github.io/Javascript30/reto16/", "https://krisg23.github.io/Javascript30/reto17/", "https://krisg23.github.io/Javascript30/reto18/", "https://krisg23.github.io/Javascript30/reto19/", "https://krisg23.github.io/Javascript30/reto20/", "https://krisg23.github.io/Javascript30/reto21/", "https://krisg23.github.io/Javascript30/reto22/", "https://krisg23.github.io/Javascript30/reto23/", "https://krisg23.github.io/Javascript30/reto24/", "https://krisg23.github.io/Javascript30/reto25/", "https://krisg23.github.io/Javascript30/reto26/", "https://krisg23.github.io/Javascript30/reto27/", "https://krisg23.github.io/Javascript30/reto28/", "https://krisg23.github.io/Javascript30/reto29/", "https://krisg23.github.io/Javascript30/reto30/"]
+var routes = ["https://krisg23.github.io/Javascript30/reto1/", "https://krisg23.github.io/Javascript30/reto2/", "https://krisg23.github.io/Javascript30/reto3/", "https://krisg23.github.io/Javascript30/reto4/", "https://krisg23.github.io/Javascript30/reto5/", "https://krisg23.github.io/Javascript30/reto6/", "https://krisg23.github.io/Javascript30/reto7/", "https://krisg23.github.io/Javascript30/reto8/", "https://krisg23.github.io/Javascript30/reto9/", "https://krisg23.github.io/Javascript30/reto10/", "https://krisg23.github.io/Javascript30/reto11/", "https://krisg23.github.io/Javascript30/reto12/", "https://krisg23.github.io/Javascript30/reto13/", "https://krisg23.github.io/Javascript30/reto14/", "https://krisg23.github.io/Javascript30/reto15/", "https://krisg23.github.io/Javascript30/reto16/", "https://krisg23.github.io/Javascript30/reto17/", "https://krisg23.github.io/Javascript30/reto18/", "https://krisg23.github.io/Javascript30/reto19/", "https://krisg23.github.io/Javascript30/reto20/", "https://krisg23.github.io/Javascript30/reto21/", "https://krisg23.github.io/Javascript30/reto22/", "https://krisg23.github.io/Javascript30/reto23/", "https://krisg23.github.io/Javascript30/reto24/", "https://krisg23.github.io/Javascript30/reto25/", "https://krisg23.github.io/Javascript30/reto26/", "https://krisg23.github.io/Javascript30/reto27/", "https://krisg23.github.io/Javascript30/reto28/", "https://krisg23.github.io/Javascript30/reto29/", "https://krisg23.github.io/Javascript30/reto30/"]
 /*function getNextMonth() {
 	monthDirection++;
 	var current;
@@ -137,7 +137,7 @@ function initCalender(monthData) {
 initCalender(getMonth(new Date()));
 
 var clickCounter = 0;
-var element = 0;
+var number = 0;
 $(".fa-angle-double-right").click(function () {
 	$(".right-wrapper").toggleClass("is-active");
 	$(this).toggleClass("is-active");
@@ -152,13 +152,11 @@ function dateClickHandler(elem) {
 		$(elem).toggleClass("active");
 		$(".right-wrapper").removeClass("is-active");
 		setTimeout(function(){
-			var number = parseInt(elem[0].innerHTML);
-			element = parseInt(elem[0].innerHTML);
+			number = parseInt(elem[0].innerHTML);
 			$(".right-wrapper .header span").html(names[number-1]);
-			$(".right-wrapper .thumb a").attr("href", routes[number]);
+			$(".right-wrapper .thumb a").attr("href", routes[number-1]);
 			$(".right-wrapper .thumb").css("background-image", "url("+thumbs[number-1]+")");
 		}, 500);
 	}
-	console.log(element);
 }
 
